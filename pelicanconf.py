3c9eb08a53947f4fdc1a640c3ce07e455c738c9b
Sinyeol An <soundlake@gmail.com>
2018-04-02 11:04:16 +0200

    Introduce Pelican System

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'soundlake'
+SITENAME = u'soundlake'
+SITEURL = 'https://soundlake.net'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Brussels'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com'),
+         ('Python.org', 'http://python.org'),
+         ('Jinja2', 'http://jinja.pocoo.org'),
+         ('Markdown', 'https://daringfireball.net/projects/markdown'),
+         ('Python-Markdown', 'https://python-markdown.github.io'),)
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/soundlake'),
+          ('stackoverflow', 'https://stackoverflow.com/users/4276533/soundlake'),
+          ('soundcloud', 'https://soundcloud.com/soundlake'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
